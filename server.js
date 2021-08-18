4d14389b0c61e1bd4d7b293a55e4d56f373a7fb7
Zach Maurer <zmaurer88@yahoo.com>
2021-08-17 19:45:55 -0700

    fix mongodb

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 app.use(express.static("public"))
 
-mongoose.connect("mongodb://localhost/fitness-tracker_db", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitness-tracker_db", {
     useNewUrlParser:true,
     useFindAndModify:false,
     useUnifiedTopology:true,
