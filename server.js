95a770d6e5c8b33ebdfea1088c758d892ab9933b
Zach Maurer <zmaurer88@yahoo.com>
2021-08-19 16:47:33 -0700

    redeploy

--- server.js
+++ server.js
@@ -5,13 +5,10 @@ const PORT = process.env.PORT || 3001;
 const app = express()
 
 app.use(logger("dev"))
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended:true }))
 app.use(express.json())
 app.use(express.static("public"))
 
-
-app.use(require("./routes/api.js"))
-app.use(require("./routes/view.js"))
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitness-tracker_db", {
     useNewUrlParser:true,
@@ -19,6 +16,10 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitness-tracker
     useUnifiedTopology:true,
     useCreateIndex:true
 })
+
+
+app.use(require("./routes/api.js"))
+app.use(require("./routes/view.js"))
 
 app.listen(PORT,() => {
     console.log(`app running on port ${PORT}`)
