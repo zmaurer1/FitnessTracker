878a99454c5eed1714762fe5b6c64eec1e2133bb
Zach Maurer <zmaurer88@yahoo.com>
2021-08-20 11:42:56 -0700

    fitness tracker fix

--- server.js
+++ server.js
@@ -21,6 +21,8 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitness-tracker
 app.use(require("./routes/api.js"))
 app.use(require("./routes/view.js"))
 
+
+
 app.listen(PORT,() => {
     console.log(`app running on port ${PORT}`)
 })
